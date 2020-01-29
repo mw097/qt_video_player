2fdcc1e65808dc4dbc6ba5434392de4dd64c9877
Jan Piotrzowski <piotrzkowski.jan@gmail.com>
2020-01-29 10:23:05 +0100

    Added table comments to sqlite db

--- build-videoplayer-Desktop_x86_windows_msvc2017_pe_32bit-Debug/debug/moc_mainwindow.cpp
+++ build-videoplayer-Desktop_x86_windows_msvc2017_pe_32bit-Debug/debug/moc_mainwindow.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Meta object code from reading C++ file 'mainwindow.h'
 **
-** Created by: The Qt Meta Object Compiler version 67 (Qt 5.12.6)
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.9.9)
 **
 ** WARNING! All changes made in this file will be lost!
 *****************************************************************************/
@@ -12,7 +12,7 @@
 #if !defined(Q_MOC_OUTPUT_REVISION)
 #error "The header file 'mainwindow.h' doesn't include <QObject>."
 #elif Q_MOC_OUTPUT_REVISION != 67
-#error "This file was generated using the moc from 5.12.6. It"
+#error "This file was generated using the moc from 5.9.9. It"
 #error "cannot be used with the include files from this version of Qt."
 #error "(The moc has changed too much.)"
 #endif
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[8];
-    char stringdata0[170];
+    QByteArrayData data[10];
+    char stringdata0[203];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -32,45 +32,56 @@ struct qt_meta_stringdata_MainWindow_t {
 static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
     {
 QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
-QT_MOC_LITERAL(1, 11, 23), // "on_actionOpen_triggered"
-QT_MOC_LITERAL(2, 35, 0), // ""
-QT_MOC_LITERAL(3, 36, 23), // "on_actionPlay_triggered"
-QT_MOC_LITERAL(4, 60, 23), // "on_actionStop_triggered"
-QT_MOC_LITERAL(5, 84, 28), // "on_actionBeginning_triggered"
-QT_MOC_LITERAL(6, 113, 31), // "on_actionAdd_Bookmark_triggered"
-QT_MOC_LITERAL(7, 145, 24) // "on_comment_btn_triggered"
+QT_MOC_LITERAL(1, 11, 16), // "onPositionChange"
+QT_MOC_LITERAL(2, 28, 0), // ""
+QT_MOC_LITERAL(3, 29, 23), // "on_actionOpen_triggered"
+QT_MOC_LITERAL(4, 53, 23), // "on_actionPlay_triggered"
+QT_MOC_LITERAL(5, 77, 23), // "on_actionStop_triggered"
+QT_MOC_LITERAL(6, 101, 28), // "on_actionBeginning_triggered"
+QT_MOC_LITERAL(7, 130, 31), // "on_actionAdd_Bookmark_triggered"
+QT_MOC_LITERAL(8, 162, 24), // "on_comment_btn_triggered"
+QT_MOC_LITERAL(9, 187, 15) // "LookForComments"
 
     },
-    "MainWindow\0on_actionOpen_triggered\0\0"
-    "on_actionPlay_triggered\0on_actionStop_triggered\0"
+    "MainWindow\0onPositionChange\0\0"
+    "on_actionOpen_triggered\0on_actionPlay_triggered\0"
+    "on_actionStop_triggered\0"
     "on_actionBeginning_triggered\0"
     "on_actionAdd_Bookmark_triggered\0"
-    "on_comment_btn_triggered"
+    "on_comment_btn_triggered\0LookForComments"
 };
 #undef QT_MOC_LITERAL
 
 static const uint qt_meta_data_MainWindow[] = {
 
  // content:
-       8,       // revision
+       7,       // revision
        0,       // classname
        0,    0, // classinfo
-       6,   14, // methods
+       8,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
        0,       // flags
-       0,       // signalCount
+       1,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    0,   54,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   44,    2, 0x08 /* Private */,
-       3,    0,   45,    2, 0x08 /* Private */,
-       4,    0,   46,    2, 0x08 /* Private */,
-       5,    0,   47,    2, 0x08 /* Private */,
-       6,    0,   48,    2, 0x08 /* Private */,
-       7,    0,   49,    2, 0x08 /* Private */,
+       3,    0,   55,    2, 0x08 /* Private */,
+       4,    0,   56,    2, 0x08 /* Private */,
+       5,    0,   57,    2, 0x08 /* Private */,
+       6,    0,   58,    2, 0x08 /* Private */,
+       7,    0,   59,    2, 0x08 /* Private */,
+       8,    0,   60,    2, 0x08 /* Private */,
+       9,    0,   61,    2, 0x08 /* Private */,
+
+ // signals: parameters
+    QMetaType::Void,
 
  // slots: parameters
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -84,29 +95,36 @@ static const uint qt_meta_data_MainWindow[] = {
 void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
 {
     if (_c == QMetaObject::InvokeMetaMethod) {
-        auto *_t = static_cast<MainWindow *>(_o);
+        MainWindow *_t = static_cast<MainWindow *>(_o);
         Q_UNUSED(_t)
         switch (_id) {
-        case 0: _t->on_actionOpen_triggered(); break;
-        case 1: _t->on_actionPlay_triggered(); break;
-        case 2: _t->on_actionStop_triggered(); break;
-        case 3: _t->on_actionBeginning_triggered(); break;
-        case 4: _t->on_actionAdd_Bookmark_triggered(); break;
-        case 5: _t->on_comment_btn_triggered(); break;
+        case 0: _t->onPositionChange(); break;
+        case 1: _t->on_actionOpen_triggered(); break;
+        case 2: _t->on_actionPlay_triggered(); break;
+        case 3: _t->on_actionStop_triggered(); break;
+        case 4: _t->on_actionBeginning_triggered(); break;
+        case 5: _t->on_actionAdd_Bookmark_triggered(); break;
+        case 6: _t->on_comment_btn_triggered(); break;
+        case 7: _t->LookForComments(); break;
         default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            typedef void (MainWindow::*_t)();
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::onPositionChange)) {
+                *result = 0;
+                return;
+            }
         }
     }
     Q_UNUSED(_a);
 }
 
-QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
-    &QMainWindow::staticMetaObject,
-    qt_meta_stringdata_MainWindow.data,
-    qt_meta_data_MainWindow,
-    qt_static_metacall,
-    nullptr,
-    nullptr
-} };
+const QMetaObject MainWindow::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_MainWindow.data,
+      qt_meta_data_MainWindow,  qt_static_metacall, nullptr, nullptr}
+};
 
 
 const QMetaObject *MainWindow::metaObject() const
@@ -128,15 +146,21 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 6)
+        if (_id < 8)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 6;
+        _id -= 8;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 6)
+        if (_id < 8)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 6;
+        _id -= 8;
     }
     return _id;
+}
+
+// SIGNAL 0
+void MainWindow::onPositionChange()
+{
+    QMetaObject::activate(this, &staticMetaObject, 0, nullptr);
 }
 QT_WARNING_POP
 QT_END_MOC_NAMESPACE
--- build-videoplayer-Desktop_x86_windows_msvc2017_pe_32bit-Debug/ui_mainwindow.h
+++ build-videoplayer-Desktop_x86_windows_msvc2017_pe_32bit-Debug/ui_mainwindow.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'mainwindow.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.6
+** Created by: Qt User Interface Compiler version 5.9.9
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -12,6 +12,8 @@
 #include <QtCore/QVariant>
 #include <QtWidgets/QAction>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QButtonGroup>
+#include <QtWidgets/QHeaderView>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
 #include <QtWidgets/QStatusBar>
@@ -38,42 +40,42 @@ public:
     void setupUi(QMainWindow *MainWindow)
     {
         if (MainWindow->objectName().isEmpty())
-            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+            MainWindow->setObjectName(QStringLiteral("MainWindow"));
         MainWindow->resize(800, 600);
-        MainWindow->setStyleSheet(QString::fromUtf8("background: #333;"));
+        MainWindow->setStyleSheet(QStringLiteral("background: #333;"));
         actionOpen = new QAction(MainWindow);
-        actionOpen->setObjectName(QString::fromUtf8("actionOpen"));
+        actionOpen->setObjectName(QStringLiteral("actionOpen"));
         actionPlay = new QAction(MainWindow);
-        actionPlay->setObjectName(QString::fromUtf8("actionPlay"));
+        actionPlay->setObjectName(QStringLiteral("actionPlay"));
         actionStop = new QAction(MainWindow);
-        actionStop->setObjectName(QString::fromUtf8("actionStop"));
+        actionStop->setObjectName(QStringLiteral("actionStop"));
         actionBeginning = new QAction(MainWindow);
-        actionBeginning->setObjectName(QString::fromUtf8("actionBeginning"));
+        actionBeginning->setObjectName(QStringLiteral("actionBeginning"));
         actionAdd_Bookmark = new QAction(MainWindow);
-        actionAdd_Bookmark->setObjectName(QString::fromUtf8("actionAdd_Bookmark"));
+        actionAdd_Bookmark->setObjectName(QStringLiteral("actionAdd_Bookmark"));
         centralwidget = new QWidget(MainWindow);
-        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        centralwidget->setObjectName(QStringLiteral("centralwidget"));
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setObjectName(QStringLiteral("menubar"));
         menubar->setGeometry(QRect(0, 0, 800, 22));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        statusbar->setObjectName(QStringLiteral("statusbar"));
         statusbar->setSizeGripEnabled(false);
         MainWindow->setStatusBar(statusbar);
         toolBar_up = new QToolBar(MainWindow);
-        toolBar_up->setObjectName(QString::fromUtf8("toolBar_up"));
-        toolBar_up->setStyleSheet(QString::fromUtf8("background: #fff;"));
+        toolBar_up->setObjectName(QStringLiteral("toolBar_up"));
+        toolBar_up->setStyleSheet(QStringLiteral("background: #fff;"));
         toolBar_up->setMovable(false);
         MainWindow->addToolBar(Qt::TopToolBarArea, toolBar_up);
         toolBar_down = new QToolBar(MainWindow);
-        toolBar_down->setObjectName(QString::fromUtf8("toolBar_down"));
-        toolBar_down->setStyleSheet(QString::fromUtf8("background: #fff;"));
+        toolBar_down->setObjectName(QStringLiteral("toolBar_down"));
+        toolBar_down->setStyleSheet(QStringLiteral("background: #fff;"));
         toolBar_down->setMovable(false);
         MainWindow->addToolBar(Qt::BottomToolBarArea, toolBar_down);
         commentBar = new QToolBar(MainWindow);
-        commentBar->setObjectName(QString::fromUtf8("commentBar"));
+        commentBar->setObjectName(QStringLiteral("commentBar"));
         commentBar->setMovable(false);
         MainWindow->addToolBar(Qt::BottomToolBarArea, commentBar);
         MainWindow->insertToolBarBreak(commentBar);
@@ -95,27 +97,27 @@ public:
 
     void retranslateUi(QMainWindow *MainWindow)
     {
-        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
-        actionOpen->setText(QApplication::translate("MainWindow", "Open", nullptr));
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", Q_NULLPTR));
+        actionOpen->setText(QApplication::translate("MainWindow", "Open", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        actionOpen->setToolTip(QApplication::translate("MainWindow", "Open A File", nullptr));
+        actionOpen->setToolTip(QApplication::translate("MainWindow", "Open A File", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        actionPlay->setText(QApplication::translate("MainWindow", "Play", nullptr));
+        actionPlay->setText(QApplication::translate("MainWindow", "Play", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        actionPlay->setToolTip(QApplication::translate("MainWindow", "Play A Video", nullptr));
+        actionPlay->setToolTip(QApplication::translate("MainWindow", "Play A Video", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        actionStop->setText(QApplication::translate("MainWindow", "Stop", nullptr));
+        actionStop->setText(QApplication::translate("MainWindow", "Stop", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        actionStop->setToolTip(QApplication::translate("MainWindow", "Stop A Video", nullptr));
+        actionStop->setToolTip(QApplication::translate("MainWindow", "Stop A Video", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        actionBeginning->setText(QApplication::translate("MainWindow", "Beginning", nullptr));
+        actionBeginning->setText(QApplication::translate("MainWindow", "Beginning", Q_NULLPTR));
 #ifndef QT_NO_TOOLTIP
-        actionBeginning->setToolTip(QApplication::translate("MainWindow", "Start Watching A Video From The Beginning", nullptr));
+        actionBeginning->setToolTip(QApplication::translate("MainWindow", "Start Watching A Video From The Beginning", Q_NULLPTR));
 #endif // QT_NO_TOOLTIP
-        actionAdd_Bookmark->setText(QApplication::translate("MainWindow", "Add Bookmark", nullptr));
-        toolBar_up->setWindowTitle(QApplication::translate("MainWindow", "toolBar", nullptr));
-        toolBar_down->setWindowTitle(QApplication::translate("MainWindow", "toolBar_2", nullptr));
-        commentBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", nullptr));
+        actionAdd_Bookmark->setText(QApplication::translate("MainWindow", "Add Bookmark", Q_NULLPTR));
+        toolBar_up->setWindowTitle(QApplication::translate("MainWindow", "toolBar", Q_NULLPTR));
+        toolBar_down->setWindowTitle(QApplication::translate("MainWindow", "toolBar_2", Q_NULLPTR));
+        commentBar->setWindowTitle(QApplication::translate("MainWindow", "toolBar", Q_NULLPTR));
     } // retranslateUi
 
 };
